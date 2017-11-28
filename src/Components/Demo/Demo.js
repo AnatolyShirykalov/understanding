66a08f4b72f1c975b44d475b2e8e3eca52a5b773
AnatolyShirykalov <pipocavsobake@gmail.com>
2017-11-28 13:34:52 +0300

    working demo

--- src/Components/Demo/Demo.js
+++ src/Components/Demo/Demo.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import classes from './Demo.css';
+import MathJax from '../../vendor/react-mathjax/src';
+
+const tex = `f(x) = \\int_{-\\infty}^\\infty
+    \\hat f(\\xi)\\,e^{2 \\pi i \\xi x}
+    \\,d\\xi`;
 
 const demo = props => (
   <div className={classes.Demo}>
-    Демо
+    <h2>Демо</h2>
+    <MathJax.Context>
+      <div>
+          This is an inline math formula: <MathJax.Node inline>{'a = b'}</MathJax.Node>
+
+          And a block one:
+
+          <MathJax.Node>{tex}</MathJax.Node>
+      </div>
+    </MathJax.Context>
   </div>
 )
 
---
+++ src/vendor/react-mathjax/README.md
@@ -0,0 +1,4 @@
+This is actually code from there
+https://github.com/SamyPesse/react-mathjax
+
+But that repository owner doesn't fix the issues.
